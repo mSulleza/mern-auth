4b0f0acccf4524a3ec712d3d0e0f7859a58b4c31
msulleza <msulleza@talinolabs.com>
2023-07-12 15:32:49 +0800

    initialized npm created routes created user model created register user controller

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// connect to MongoDB
+connectDB();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+// routes
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+});
+
+// custom error handler
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
